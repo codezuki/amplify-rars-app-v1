0f50442e4a7b8400d35a00e1b5566c5ab3f3cc52
codezuki <erfrcse@gmail.com>
2026-01-19 17:58:58 +0530

    Downgrade package-lock to v2 for AWS Amplify compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
-import awsExports from '../aws-exports'
+import awsExports from './aws-exports'
 import App from './App'
 import './index.css'
 Amplify.configure(awsExports)
